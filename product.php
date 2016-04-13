6b25f5018af75101ad94339273951816934cf61b
Johnathon <johnnymacdon@hotmail.com>
2016-04-13 20:13:48 +1000

    Display database
    code to display data from the database to screen

--- product.php
+++ product.php
@@ -1,5 +1,5 @@
 <?php 
-	//customer
+	// product
     require_once ("settings.php");
  
     $conn = @mysqli_connect($mysql_host,
@@ -17,16 +17,16 @@
     {
         echo "<p>Connected to the database <b>$sql_db</b></p>";
 
-	echo"<title>Customer</title>";
+	echo"<title>Product</title>";
 	echo"<h1>People Health Pharmacy Sales Reporting System</h1>";
-	echo '<li><a href="index.php" target="_blank">'. $post['message']. '<b>Customer</b></a></li>';
+	echo '<li><a href="index.php" target="_blank">'. $post['message']. 'Customer</a></li>';
 	echo '<li><a href="order.php" target="_blank">'. $post['message']. 'Order</a></li>';
-	echo '<li><a href="product.php" target="_blank">'. $post['message']. 'Product</a></li>';
+	echo '<li><a href="product.php" target="_blank">'. $post['message']. '<b>Product</b></a></li>';
 	echo '<li><a href="sale.php" target="_blank">'. $post['message']. 'Sale</a></li>';
  
-	echo"<h2>Customer Database</h2>";
+	echo"<h2>Product Database</h2>";
 	
-	$sql = 'SELECT cid, cname, cgender, cage, cphone FROM CUSTOMER';
+	$sql = 'SELECT pid, pname, pdesc, pwholesale, pselling, pstock FROM PRODUCT';
 	
 	$result = mysqli_query($conn, $sql);
 
@@ -37,15 +37,20 @@
     	{
     	
     		
-   		echo "Customer ID :{$row["cid"]}  <br> ".
-         	"Customer Name: {$row["cname"]} <br> ".
-         	"Customer Gender: {$row["cgender"]} <br> ".
-	        "Customer Age: {$row["cage"]} <br> ".
-         	"Customer Phone: {$row["cphone"]} <br> ".
+   		echo "Product ID :{$row["pid"]}  <br> ".
+         	"Product Name: {$row["pname"]} <br> ".
+         	"Product Description: {$row["pdesc"]} <br> ".
+	        "Product Wholesale Price: {$row["pwholesale"]} <br> ".
+         	"Product Selling Price: {$row["pselling"]} <br> ".
+		"Product Stock: {$row["pstock"]} <br> ".
          	"------------------<br>";
     	
 	}
 	
+	}
+	else
+	{
+		echo"database connected but failed to display";
 	}
 	
     }
